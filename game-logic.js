50f390678faba2217f8a1f5959a88855e5450bf1
Claudia El Khatib <claudiacv22@gmail.com>
2018-08-05 20:03:01 -0400

    created function to form a word and sum the total of points

--- game-logic.js
+++ game-logic.js
@@ -9,17 +9,7 @@
 // Create a function that defines the rules to follow and have a winner
 // Create a function to reset the score or start a new game
 
-
-// Game board boxes
-let arrNum = 225;
-const gameBoardBoxes = Array.from(Array(225), (d, i) => i);
-console.log("hola ", gameBoardBoxes);
-console.log("length", gameBoardBoxes.length);
-
-// Assigning values to special game board boxes
-
-
-// letters
+// Object with letters and points of each one.
 const letters = {
   noLetter: 0,
   a: 1,
@@ -32,7 +22,6 @@ const letters = {
   h: 4,
   i: 1,
   j: 8,
-  k: 8,
   l: 1,
   ll: 8,
   m: 3,
@@ -50,13 +39,72 @@ const letters = {
   y: 4,
   z: 10
 };
-console.log('letters ', letters);
+//console.log('letters ', letters);
 
-// 2 fichas en blanco (0 puntos)
-// 1 punto: A ×12, E ×12, O ×9, I ×6, S ×6, N ×5, L ×4, R ×5, U ×5, T ×4
-// 2 puntos: D ×5, G ×2
-// 3 puntos: C ×4, B ×2, M ×2, P ×2
-// 4 puntos: H ×2, F ×1, V ×1, Y ×1
-// 5 puntos: CH ×1, Q ×1
-// 8 puntos: J ×1, LL ×1, Ñ ×1, RR ×1, X ×1
-// 10 puntos: Z ×1,
+// Get points for each letter selected to form a word and return the total points of the word formed.
+let totalWordPoints = 0;
+const selectedLetter = function(letter) {
+  let valueKey = "";
+  for (var key in letters) {
+    //console.log(`${key}: ${letters[key]}`);
+    if (key === letter) {
+      totalWordPoints += letters[key];
+      console.log(`Match! key: ${key} letter: ${letter} valueKey: ${valueKey}`);
+      console.log(`Total word points ${totalWordPoints} `)
+      return totalWordPoints;
+    }
+  }
+};
+
+selectedLetter("s");
+selectedLetter("i");
+
+// Limitar las veces que una letra puede ser seleccionada
+
+// Reset totalWordPoints function
+// const resettotalWordPoints = function() {
+//   if () {
+//     return totalWordPoints = 0;
+//   }
+// };
+
+
+
+// Invocar la selectedLetter function por cada letra escogida
+// Tomar ese valor y sumarle cada valor en otra variable
+
+
+// Distribute the letter to each player
+// first round --> 7 lettres
+// Following rounds --> number letters used === number letters to take
+
+
+// Chosing letters function
+// function chosingLetters() {
+// iterate in the array to find the letter.
+// Example: casa
+// }
+// Checking if the box is empty and accept letter
+
+
+// Game board boxes
+let arrNum = 225;
+const gameBoardBoxes = Array.from(Array(225), (d, i) => i);
+// console.log("hola ", gameBoardBoxes);
+// console.log("length", gameBoardBoxes.length);
+
+// Assigning values to special game board boxes
+// function specialGameBoardBoxes() {
+//   if (red_box is checked) {
+//       multiplicar el total de la palabra x 3
+//   }
+//   if (pink_box is checked) {
+//       multiplicar el total de la palabra x 3
+//   }
+//   if (dark_blue_box is checked) {
+//     multiplicar la letra x 3
+//   }
+//   if (light_blue_box is checked) {
+//     multiplicar letra x2
+//   }
+// }
