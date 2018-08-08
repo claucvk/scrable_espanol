241e7622ab3b552cc3e47dd8bcd62cf898cfcd3d
Claudia El Khatib <claudiacv22@gmail.com>
2018-08-07 20:14:24 -0400

    Updated function to define the number of letters available to use

--- game-logic.js
+++ game-logic.js
@@ -9,46 +9,46 @@
 // Create a function that defines the rules to follow and have a winner
 // Create a function to reset the score or start a new game
 
-// Object with letters and points of each one.
+// Object value[0] = point of the letters - value[1] = number of letters available
 const letters = {
   noLetter: 0,
-  a: 1,
-  b: 3,
-  c: 3,
-  d: 2,
-  e: 1,
-  f: 4,
-  g: 2,
-  h: 4,
-  i: 1,
-  j: 8,
-  l: 1,
-  ll: 8,
-  m: 3,
-  n: 1,
-  ñ: 8,
-  o: 1,
-  p: 3,
-  q: 5,
-  r: 1,
-  s: 1,
-  t: 1,
-  u: 1,
-  v: 4,
-  x: 8,
-  y: 4,
-  z: 10
+  a: [1, 12],
+  b: [3, 2],
+  c: [3, 4],
+  d: [2, 5],
+  e: [1, 12],
+  f: [4, 1],
+  g: [2, 2],
+  h: [4, 2],
+  i: [1, 6],
+  j: [8, 1],
+  l: [1, 4],
+  ll: [8, 1],
+  m: [3, 2],
+  n: [1, 5],
+  ñ: [8, 1],
+  o: [1, 9],
+  p: [3, 2],
+  q: [5, 1],
+  r: [1, 5],
+  s: [1, 6],
+  t: [1, 4],
+  u: [1, 5],
+  v: [4, 1],
+  x: [8, 1],
+  y: [4, 1],
+  z: [10, 1]
 };
-//console.log('letters ', letters);
 
 // Get points for each letter selected to form a word and return the total points of the word formed.
 let totalWordPoints = 0;
 const selectedLetter = function(letter) {
   let valueKey = "";
+  let subtractNumberOfLetters = letters[letter] = [letters[letter][0], letters[letter][1] - 1];
+  console.log("numero de letras ", subtractNumberOfLetters)
   for (var key in letters) {
-    //console.log(`${key}: ${letters[key]}`);
     if (key === letter) {
-      totalWordPoints += letters[key];
+      totalWordPoints += letters[key][0];
       console.log(`Match! key: ${key} letter: ${letter} valueKey: ${valueKey}`);
       console.log(`Total word points ${totalWordPoints} `)
       return totalWordPoints;
@@ -60,6 +60,11 @@ selectedLetter("s");
 selectedLetter("i");
 
 // Limitar las veces que una letra puede ser seleccionada
+  // Cada vez que salga la letra se le resta 1
+  // const numberOfLetters = function (letter) {
+  //   let a = 12;
+  //   if ()
+  // };
 
 // Reset totalWordPoints function
 // const resettotalWordPoints = function() {
