ed3d964a60505b7da89e3b0cd19cb9695dc5461e
Claudia El Khatib <claudiacv22@gmail.com>
2019-01-30 16:35:20 -0800

    Selecting multiple random items (letters) from an array

--- game-logic.js
+++ game-logic.js
@@ -6,48 +6,71 @@
 // Create a function that defines the rules to follow and have a winner
 // Create a function to reset the score or start a new game
 
+// noLetter: [0, 2],
+// a: [1, 12],
+// b: [3, 2],
+// c: [3, 4],
+// ch: [5, 1],
+// d: [2, 5],
+// e: [1, 12],
+// f: [4, 1],
+// g: [2, 2],
+// h: [4, 2],
+// i: [1, 6],
+// j: [8, 1],
+// l: [1, 4],
+// ll: [8, 1],
+// m: [3, 2],
+// n: [1, 5],
+// ñ: [8, 1],
+// o: [1, 9],
+// p: [3, 2],
+// q: [5, 1],
+// r: [1, 5],
+// rr: [8, 1],
+// s: [1, 6],
+// t: [1, 4],
+// u: [1, 5],
+// v: [4, 1],
+// x: [8, 1],
+// y: [4, 1],
+// z: [10, 1]
+
 // Players --> player1 = 0 | player2 = 1
 let currentPlayer;
 // Get points for each letter selected to form a word and return the total points of the word formed.
 let totalWordPointsPlayer1 = 0;
 let totalWordPointsPlayer2 = 0;
-// Object value[0] = letters points -- value[1] = quantity of letters available
-const letters = {
-  noLetter: [0, 2],
-  a: [1, 12],
-  b: [3, 2],
-  c: [3, 4],
-  ch: [5, 1],
-  d: [2, 5],
-  e: [1, 12],
-  f: [4, 1],
-  g: [2, 2],
-  h: [4, 2],
-  i: [1, 6],
-  j: [8, 1],
-  l: [1, 4],
-  ll: [8, 1],
-  m: [3, 2],
-  n: [1, 5],
-  ñ: [8, 1],
-  o: [1, 9],
-  p: [3, 2],
-  q: [5, 1],
-  r: [1, 5],
-  rr: [8, 1],
-  s: [1, 6],
-  t: [1, 4],
-  u: [1, 5],
-  v: [4, 1],
-  x: [8, 1],
-  y: [4, 1],
-  z: [10, 1]
+
+const letters = ["noLetter", "noLetter", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "b", "b", "c", "c", "c", "c", "ch", "d", "d", "d", "d", "d", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "f", "g", "g", "h", "h", "i", "i", "i", "i", "i", "i", "j", "l", "l", "l", "l", "ll", "m", "m", "n", "n", "n", "n", "n", "ñ", "o", "o", "o", "o", "o", "o", "o", "o", "o", "p", "p", "q", "r","r", "r", "r", "r", "rr", "s", "s", "s", "s", "s", "s", "t", "t", "t", "t", "u", "u", "u", "u", "u", "v", "x", "y", "z"];
+
+// Distributing the letters to each player (2 players)
+// Keeping tracking of the letters UNUSED -- DISTRIBUTED -- USED.
+  // Subtracting the quantity of letters taken from the lettersUnused group (letters bag).
+  // Adding the letters substracted from the lettersUnused group to the lettersDistributed group.
+  // Counting the letters letters used/played (lettresPlayed) to determine how many letters to take from the the lettersUnused group.
+// Initial round --> Take 14 letters (7 for each player) from the lettersUnused = 100
+// Following rounds --> quantity of letters to distribute (lettersDistributed) depends on the quantity of letters used/played (lettresPlayed). The lettersDistributed are taken from the lettersUnused group.
+const lettersUnused = letters.length;
+
+const selectingRandomLetter = function () {
+  const oneRandomLetter = letters[Math.floor(Math.random() * letters.length)];
+  console.log('oneRandomLetter ', oneRandomLetter);
 };
 
-let initialSumOfLetters = Object.keys(letters).reduce(function(previous, key) {
-  return previous + letters[key][1];
-}, 0);
-console.log('initialSumOfLetters out ', initialSumOfLetters);
+selectingRandomLetter();
+
+const multipleRandomLetters = function (numberOfLetterstoTake) {
+  for (let i = 0; i < numberOfLetterstoTake - 1; i++) {
+   selectingRandomLetter();
+  }
+};
+
+multipleRandomLetters(7);
+
+// push the random generated letters from the letters array
+// make a new array witht the letters random generated
+
 
 // Counting points earned by each letter used for each player.
 // Keeping record of letters used.
@@ -74,24 +97,7 @@ console.log('initialSumOfLetters out ', initialSumOfLetters);
 //   }
 // };
 
-// Distributing the letters to each player (2 players)
-// Keeping tracking of the letters UNUSED -- DISTRIBUTED -- USED.
-  // Subtracting the quantity of letters taken from the lettersUnused group (letters bag).
-  // Adding the letters substracted from the lettersUnused group to the lettersDistributed group.
-  // Counting the letters letters used/played (lettresPlayed) to determine how many letters to take from the the lettersUnused group.
-// Initial round --> Take 14 letters (7 for each player) from the lettersUnused = 100
-// Following rounds --> quantity of letters to distribute (lettersDistributed) depends on the quantity of letters used/played (lettresPlayed). The lettersDistributed are taken from the lettersUnused group.
 
-const lettersDistrubution = function () {
-  console.log('initialSumOfLetters in function', initialSumOfLetters);
-  if (initialSumOfLetters === 100) {
-    const objToArr = Object.entries(letters);
-    const pickRandomLetters = Array.splice({length:7}, () => objToArr[Math.floor(Math.random() * objToArr.length)]);
-    console.log('pickRandomLetters ', pickRandomLetters);
-    return pickRandomLetters;
-  }
-}
-lettersDistrubution();
 
 // // Player 1
 // const player1Moves = function () {
